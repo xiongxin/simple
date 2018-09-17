390f5b24acc916a4662be28c560132d53c20b1f2
熊鑫 <1781189926@qq.com>
2018-09-17 09:32:52 +0800

    first commit

---
+++ simple.rb
@@ -0,0 +1,144 @@
+# 变量类型
+class Variable < Struct.new(:name)
+    def to_s
+        name.to_s
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce(environment)
+        environment[name]
+    end
+end
+
+#Boolean
+class Boolean < Struct.new(:value)
+    def to_s
+        value.to_s
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        false
+    end
+end
+
+# 数
+class Number < Struct.new(:value)
+    def to_s
+        "#{value}"
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        false
+    end
+end
+
+#小于判断
+class LessThan < Struct.new(:left, :right)
+    def to_s
+        "#{left} < #{right}"
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce
+        if left.reducible?
+            LessThan.new(left.reduce, right)
+        elsif right.reducible?
+            LessThan.new(left, right.reduce)
+        else
+            Boolean.new(left.value < right.value)
+        end
+    end
+end
+
+# 加法
+class Add < Struct.new(:left, :right)
+    def to_s
+        "#{left} + #{right}"
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce
+        if left.reducible?
+            Add.new( left.reduce, right )
+        elsif right.reducible?
+            Add.new( left, right.reduce )
+        else
+            Number.new(left.value + right.value)
+        end
+    end
+end
+
+# 乘法
+class Multiply < Struct.new(:left, :right)
+    def to_s
+        "#{left} * #{right}"
+    end
+
+    def inspect
+        "<<#{self}>>"
+    end
+
+    def reducible?
+        true
+    end
+
+    def reduce
+        if left.reducible?
+            Multiply.new( left.reduce, right )
+        elsif right.reducible?
+            Multiply.new( left, right.reduce )
+        else
+            Number.new( left.value * right.value )
+        end
+    end
+end
+
+# VM
+class Machine < Struct.new(:expression)
+    def step
+        self.expression = expression.reduce
+    end
+
+    def run
+        while expression.reducible?
+            puts expression
+            step
+        end
+        puts expression
+    end
+end
+
+expression = Machine.new(Add.new(          Multiply.new(Number.new(1), Number.new(2)),          Multiply.new(Number.new(3), Number.new(4))        )) 
+expression.run
+
+expression = Machine.new( LessThan.new( Number.new(5), Add.new( Number.new(2), Number.new(2) ) ) )
+expression.run
